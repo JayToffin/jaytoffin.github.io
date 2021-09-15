122990a9034e516b6dcf5afbe445ae583053a19c
rendibgs <rendibagus56@gmail.com>
2021-09-15 16:51:52 +0700

    update

--- static-e-commerce/js/main.js
+++ static-e-commerce/js/main.js
@@ -110,30 +110,26 @@
 	};
 	carousel();
 
-	$( document ).ready( function () {
-		$( '.dropdown-menu a.dropdown-toggle' ).on( 'click', function ( e ) {
-			var $el = $( this );
-			var $parent = $( this ).offsetParent( ".dropdown-menu" );
-			if ( !$( this ).next().hasClass( 'show' ) ) {
-				$( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
-			}
-			var $subMenu = $( this ).next( ".dropdown-menu" );
-			$subMenu.toggleClass( 'show' );
-			
-			$( this ).parent( "li" ).toggleClass( 'show' );
-	
-			$( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
-				$( '.dropdown-menu .show' ).removeClass( "show" );
-			} );
-			
-			 if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
-				$el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
-			}
-	
-			return false;
-		} );
-	} );
 
+
+	// $('nav .dropdown').hover(function(){
+	// 	var $this = $(this);
+	// 	// 	 timer;
+	// 	// clearTimeout(timer);
+	// 	$this.addClass('show');
+	// 	$this.find('> a').attr('aria-expanded', true);
+	// 	// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+	// 	$this.find('.dropdown-menu').addClass('show');
+	// }, function(){
+	// 	var $this = $(this);
+	// 		// timer;
+	// 	// timer = setTimeout(function(){
+	// 		$this.removeClass('show');
+	// 		$this.find('> a').attr('aria-expanded', false);
+	// 		// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+	// 		$this.find('.dropdown-menu').removeClass('show');
+	// 	// }, 100);
+	// });
 
 	// scroll
 	var scrollWindow = function() {
