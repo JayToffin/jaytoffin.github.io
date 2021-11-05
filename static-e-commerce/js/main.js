50868c7420a47d80c08b727a8aa0c77d211f3655
rendibagus <you@example.com>
2021-11-05 15:06:35 +0700

    template email delivery

--- static-e-commerce/js/main.js
+++ static-e-commerce/js/main.js
@@ -313,3 +313,18 @@
 
 })(jQuery);
 
+
+
+// Breadcrumbs
+$(document).ready(function () {
+
+
+	$('.first ol li a').click(function () {
+	$('.first ol li a.active-1').removeClass('active-1');
+	$(this).closest('a').addClass('active-1');
+	});
+  
+  
+  
+	});
+
