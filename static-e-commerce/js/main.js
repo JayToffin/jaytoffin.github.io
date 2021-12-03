5cb62d969f611f5e079546d09296f56d3eb0a38e
rendibagus <you@example.com>
2021-12-03 09:23:11 +0700

    update checkout

--- static-e-commerce/js/main.js
+++ static-e-commerce/js/main.js
@@ -328,3 +328,29 @@ $(document).ready(function () {
   
 	});
 
+
+
+
+
+
+	
+	function toggleActive(self) {
+		let ul = self.getElementsByTagName('ul')[0];
+		if(ul.classList.contains('active')) {
+		  ul.classList.remove('active');
+		} else {
+		  ul.classList.add('active');
+		}
+	  }
+	  
+	  function setTeam(self) {
+		let team = document.getElementById('current-team');
+		let node = self.parentNode.firstChild;
+		while( node && node.nodeType === Node.ELEMENT_NODE && node !== this ) {
+		  node.classList.remove('has-text-weight-semibold');
+		  node = node.nextElementSibling || node.nextSibling;
+		}
+		self.classList.add('has-text-weight-semibold');
+		team.innerHTML = self.innerHTML;
+	  }
+	  
