da5b963bf84c79e9c12dcb7b7689e9cc86129ee5
Rendi Bagus <rendi@toffin.id>
2022-02-17 17:06:36 +0700

    update static menu

--- static-e-commerce/js/main.js
+++ static-e-commerce/js/main.js
@@ -573,3 +573,43 @@ $(".reveal").on('click',function() {
 // 	}
 // });
 // -----END - js carousel rekomendasi, alternatif product, dll ------ //
+
+
+
+
+var swiper = new Swiper('.blog-slider', {
+	spaceBetween: 30,
+	effect: 'fade',
+	loop: true,
+	mousewheel: {
+	  invert: false,
+	},
+	// autoHeight: true,
+	pagination: {
+	  el: '.blog-slider__pagination',
+	  clickable: true,
+	}
+  });
+
+
+
+
+var swiper = new Swiper( '.swiper-container.two', {
+	pagination: '.swiper-pagination',
+	paginationClickable: true,
+	effect: 'coverflow',
+	loop: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	coverflow: {
+		rotate: 0,
+		stretch: 100,
+		depth: 150,
+		modifier: 1.5,
+		slideShadows : false,
+	}
+} );
+
+
+
+
