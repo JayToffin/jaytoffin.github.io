5786760cafd2ff350dc41f60016b35d3f0dd4d83
Rendi Bagus <rendi@toffin.id>
2022-04-22 13:47:55 +0700

    update css transform slider

--- static-e-commerce/js/main.js
+++ static-e-commerce/js/main.js
@@ -639,38 +639,38 @@ function onlyNumberKey(e) {
 
 
 
-const sliders = document.querySelectorAll('.card-container');
-let isDown = false;
-let startX;
-let scrollLeft;
+// const sliders = document.querySelectorAll('.card-container');
+// let isDown = false;
+// let startX;
+// let scrollLeft;
 
-sliders.forEach(slider => {
-	slider.addEventListener('mousedown', (e) => {
-		isDown = true;
-		slider.classList.add('active');
-		startX = e.pageX - slider.offsetLeft;
-		scrollLeft = slider.scrollLeft;
-	});
-	slider.addEventListener('mouseleave', () => {
-		isDown = false;
-		slider.classList.remove('active');
-	});
-	slider.addEventListener('mouseup', () => {
-		isDown = false;
-		slider.classList.remove('active');
-	});
-	slider.addEventListener('mousemove', (e) => {
-		if (!isDown) return;
-		e.preventDefault();
-		const x = e.pageX - slider.offsetLeft;
-		const walk = (x - startX) * 1; //scroll-fast
-		slider.scrollLeft = scrollLeft - walk;
-		const links = slider.querySelectorAll('.item');
-		for (var i = 0; i < links.length; i++) {
-			links[i].classList.add('noclick');
-		}
-	});
-});
+// sliders.forEach(slider => {
+// 	slider.addEventListener('mousedown', (e) => {
+// 		isDown = true;
+// 		slider.classList.add('active');
+// 		startX = e.pageX - slider.offsetLeft;
+// 		scrollLeft = slider.scrollLeft;
+// 	});
+// 	slider.addEventListener('mouseleave', () => {
+// 		isDown = false;
+// 		slider.classList.remove('active');
+// 	});
+// 	slider.addEventListener('mouseup', () => {
+// 		isDown = false;
+// 		slider.classList.remove('active');
+// 	});
+// 	slider.addEventListener('mousemove', (e) => {
+// 		if (!isDown) return;
+// 		e.preventDefault();
+// 		const x = e.pageX - slider.offsetLeft;
+// 		const walk = (x - startX) * 1; //scroll-fast
+// 		slider.scrollLeft = scrollLeft - walk;
+// 		const links = slider.querySelectorAll('.item');
+// 		for (var i = 0; i < links.length; i++) {
+// 			links[i].classList.add('noclick');
+// 		}
+// 	});
+// });
 
 
 
