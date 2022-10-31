09696e66facaf77f8933122dde16e5be80c8a3ec
Rendi Bagus <rendi@toffin.id>
2022-10-31 13:04:13 +0700

    copy to clipboard

--- static-e-commerce/js/main.js
+++ static-e-commerce/js/main.js
@@ -3,6 +3,46 @@ AOS.init({
 	easing: 'slide'
 });
 
+
+const links = document.querySelectorAll('.copy-click');
+						const cls = {
+						  copied: 'is-copied',
+						  hover: 'is-hovered' };
+						
+						
+						const copyToClipboard = str => {
+						  const el = document.createElement('input');
+						  str.dataset.copyString ? el.value = str.dataset.copyString : el.value = str.text;
+						  el.setAttribute('readonly', '');
+						  el.style.position = 'absolute';
+						  el.style.opacity = 0;
+						  document.body.appendChild(el);
+						  el.select();
+						  document.execCommand('copy');
+						  document.body.removeChild(el);
+						};
+						
+						const clickInteraction = e => {
+						  e.preventDefault();
+						  copyToClipboard(e.target);
+						  e.target.classList.add(cls.copied);
+						  setTimeout(() => e.target.classList.remove(cls.copied), 1000);
+						  setTimeout(() => e.target.classList.remove(cls.hover), 700);
+						};
+						
+						Array.from(links).forEach(link => {
+						  link.addEventListener('click', e => clickInteraction(e));
+						  link.addEventListener('keypress', e => {
+							if (e.keyCode === 13) clickInteraction(e);
+						  });
+						  link.addEventListener('mouseover', e => e.target.classList.add(cls.hover));
+						  link.addEventListener('mouseleave', e => {
+							if (!e.target.classList.contains(cls.copied)) {
+							  e.target.classList.remove(cls.hover);
+							}
+						  });
+						});
+
 (function ($) {
 
 	"use strict";
@@ -846,3 +886,24 @@ $(document).ready(function() {
       );
     });
 });
+
+function copy(copyId){
+    let inputElement = document.createElement("input");
+    inputElement.type = "text";
+    let copyText = document.getElementById(copyId).innerHTML;
+    inputElement.value = copyText;
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputElement);
+    
+
+	document.getElementById("alertcopy-resi").style.display = "block";
+    setTimeout(function(){
+        document.getElementById("alertcopy-resi").style.display = "none";
+    }, 1000);
+}
+
+
+
+
