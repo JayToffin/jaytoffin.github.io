b68f839ed94632af709891b959a4f82497121a4d
Rendi Bagus <rendi@toffin.id>
2023-03-10 10:42:03 +0700

    global footer, header

---
+++ Smeg-Indonesia/news.php
@@ -0,0 +1,87 @@
+<?php 
+include 'header.php';
+?>
+
+	<section class="settopnewsevent">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="setnewstextbreadcrumbleft mg-0">NEWS & EVENT
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="container-fluid">
+			<div class="row mt-md-5 setmglr80res">
+				<div class='col-lg-12 mgt-20'>
+					<div class='row'>
+						<div class='col-lg-6 pdlr-0 setheightnewspixall'>
+							<a href='news-smeg-sial-2022.php'><img src='asset/image/blog/sial-interfood.jpg' class='img-newslist'
+									alt='SMEG MADE MY DAY PHOTO EXHIBITION'></a>
+						</div>
+						<div class='col-lg-6 setnewsbordercolright setheightnewspixall pl-5'>
+							<p class='mgt-50 setnewstextcatemain'>News</p><a class='title-news'
+								href='news-smeg-sial-2022.php'>
+								<h2 class='setnewstextheadline'>SMEG FIRST APPEARANCE - SIAL INTERFOOD 2022</h2>
+							</a>
+							<!-- <p class='setnewstextdate'>08&nbsp;&nbsp;<span
+									class='setbulletdate'>&#9679;</span>&nbsp;&nbsp;13&nbsp;&nbsp;<span
+									class='setbulletdate'>&#9679;</span>&nbsp;&nbsp;2022</p> -->
+							<a href='news-smeg-sial-2022.php'
+								class='setpointer setnewsbtnreadmore'>Read more</a>
+						</div>
+					</div>
+				</div>
+				<!-- <div class='col-lg-12 mgt-20'>
+					<div class='row'>
+						<div class='col-lg-6 pdlr-0 setheightnewspixall'>
+							<a href='#'><img
+									src='asset/image/blog/news.jpg' class='img-newslist'
+									alt='SMEG ATTENDS EUROCUCINA 2022'></a>
+						</div>
+						<div class='col-lg-6 setnewsbordercolright setheightnewspixall pl-5'>
+							<p class='mgt-50 setnewstextcatemain'>News</p><a class='title-news'
+								href='#'>
+								<h2 class='setnewstextheadline'>SMEG ATTENDS EUROCUCINA 2022</h2>
+							</a>
+							<p class='setnewstextdate'>07&nbsp;&nbsp;<span
+									class='setbulletdate'>&#9679;</span>&nbsp;&nbsp;25&nbsp;&nbsp;<span
+									class='setbulletdate'>&#9679;</span>&nbsp;&nbsp;2022</p>
+							<a href='#' class='setpointer setnewsbtnreadmore'>Read
+								more</a>
+						</div>
+					</div>
+				</div>
+				<div class='col-lg-12 mgt-20'>
+					<div class='row'>
+						<div class='col-lg-6 pdlr-0 setheightnewspixall'>
+							<a href='#'><img
+									src='asset/image/blog/news.jpg'
+									class='img-newslist' alt='Smeg Made my day Playlist by Yong Armchair'></a>
+						</div>
+						<div class='col-lg-6 setnewsbordercolright setheightnewspixall pl-5'>
+							<p class='mgt-50 setnewstextcatemain'>News</p><a class='title-news'
+								href='#'>
+								<h2 class='setnewstextheadline'>Smeg Made my day Playlist by Yong Armchair</h2>
+							</a>
+							<p class='setnewstextdate'>06&nbsp;&nbsp;<span
+									class='setbulletdate'>&#9679;</span>&nbsp;&nbsp;01&nbsp;&nbsp;<span
+									class='setbulletdate'>&#9679;</span>&nbsp;&nbsp;2021</p>
+							<a href='#'
+								class='setpointer setnewsbtnreadmore'>Read more</a>
+						</div>
+					</div>
+				</div> -->
+				
+
+			</div>
+
+		</div>
+	</section>
+
+<?php 
+include 'footer.php';
+?>
