5bee25c6df55cdff3d87ccfb6e81292312964322
Rendi Bagus <rendibagus56@gmail.com>
2024-03-25 11:31:07 +0700

    preloader

--- toffin-ocs/js/script.js
+++ toffin-ocs/js/script.js
@@ -339,56 +339,60 @@
     })(jQuery);
 
 
-/* ======
-   When document is ready, do
-   ====== */
-    // $(document).on('ready', function() {
-    //     // add your functions
-    //     navbarScrollfixed();
-    //     scrollToTop();
-    //     mobileNavToggle();
-    // });
+// /* ======
+//    When document is ready, do
+//    ====== */
+    $(document).on('ready', function() {
+        // add your functions
+        navbarScrollfixed();
+        scrollToTop();
+        mobileNavToggle();
+    });
     
-/* ======
-   When document is loading, do
-   ====== */
+// /* ======
+//    When document is loading, do
+//    ====== */
     // window on Load function
-    // $(window).on('load', function() {
-    //     // add your functions
-    //     preloaderLoad();
-    // });
-    // // window on Scroll function
-    // $(window).on('scroll', function() {
-    //     // add your functions
-    // });
+    $(window).on('load', function() {
+        // add your functions
+        preloaderLoad();
+    });
+    // window on Scroll function
+    $(window).on('scroll', function() {
+        // add your functions
+    });
+
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
 
 
 })(window.jQuery);
 
 
-// window.addEventListener('DOMContentLoaded', event => {
 
-//     // Activate Bootstrap scrollspy on the main nav element
-//     const mainNav = document.body.querySelector('#mainNav');
-//     if (mainNav) {
-//         new bootstrap.ScrollSpy(document.body, {
-//             target: '#mainNav',
-//             rootMargin: '0px 0px -40%',
-//         });
-//     };
-
-//     // Collapse responsive navbar when toggler is visible
-//     const navbarToggler = document.body.querySelector('.navbar-toggler');
-//     const responsiveNavItems = [].slice.call(
-//         document.querySelectorAll('#navbarResponsive .nav-link')
-//     );
-//     responsiveNavItems.map(function (responsiveNavItem) {
-//         responsiveNavItem.addEventListener('click', () => {
-//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-//                 navbarToggler.click();
-//             }
-//         });
-//     });
-
-// });
 
